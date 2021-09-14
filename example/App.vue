e634869ddab4699bca07ea6988b7df2587cd8762
博文 <zhaobowen@mockuai.com>
2021-09-14 10:32:40 +0800

    docs: update

--- example/App.vue
+++ example/App.vue
@@ -1,14 +1,53 @@
 <template>
   <fieldset>
-    <legend>示例</legend>
-    <button @click="onDownloadHTML">下载 HTML</button>
-    ｜
-    <Download
-      href="https://dummyimage.com/100x100/894FC4/FFF.png?text=hello"
-      >下载图片</Download
+    <legend>
+      普通 a 标签下载：不支持跨域重命名；甚至直接打开预览而不是下载
+    </legend>
+
+    <a
+      href="https://dummyimage.com/300x300/92cbff/fff"
+      >图片</a
     >
     ｜
-    <Download href="//httpbin.org/get">下载json</Download>
+    <a href="//httpbin.org/get">JSON</a>
+    ｜
+    <a
+      href="https://wdzg-file-pre-cdn.mockuai.com/picture/open_file202109091739144850000165.6版本).pdf"
+      download="aa.pdf"
+    >
+      PDF
+    </a>
+    ｜
+    <a
+      href="https://wdzg-file-pre-cdn.mockuai.com/picture/open_file202109131633158710000670.xlsx"
+      download="aa.xlsx"
+    >
+      表格
+    </a>
+  </fieldset>
+
+  <fieldset>
+    <legend>使用此方法下载</legend>
+    <button @click="onDownloadHTML">HTML</button>
+    ｜
+    <Download
+      href="https://dummyimage.com/300x300/92cbff/fff"
+      >图片</Download
+    >
+    ｜
+    <Download
+      href="https://wdzg-file-pre-cdn.mockuai.com/picture/open_file202109091739144850000165.6版本).pdf"
+      download="aa.pdf"
+      >PDF</Download
+    >
+    ｜
+    <Download href="//httpbin.org/get">JSON</Download>
+    ｜
+    <Download
+      href="https://wdzg-file-pre-cdn.mockuai.com/picture/open_file202109131633158710000670.xlsx"
+      download="aa.xlsx"
+      >表格</Download
+    >
     ｜
     <Download
       href="https://raw.githubusercontent.com/bowencool/download-by-url/main/example/App.vue"
--- example/download.vue
+++ example/download.vue
@@ -1,24 +1,49 @@
-<script>
-import saveFileByUrl  from '../src';
-import { defineComponent } from 'vue';
+<template>
+  <component :is="tag" v-bind="$attrs" @click="onClick"><slot /></component>
+</template>
 
-export default defineComponent({
-  props: {
-    href: String,
-    download: String,
-  },
-  setup(props) {
-    return {
-      onDownload() {
-        if (props.href) {
-          saveFileByUrl(props.href, props.download);
+<script>
+  import { defineComponent } from 'vue';
+  import downloadByUrl from '../src';
+
+  export default defineComponent({
+    name: 'Download',
+    emits: {
+      success: null,
+      error: null,
+    },
+    props: {
+      tag: {
+        type: String,
+        default: 'a',
+      },
+      url: {
+        type: [String, URL],
+      },
+      href: {
+        type: [String, URL],
+      },
+      src: {
+        type: [String, URL],
+      },
+      filename: {
+        type: String,
+      },
+      download: {
+        type: String,
+      },
+    },
+    methods: {
+      onClick() {
+        const url = this.href || this.src || this.url;
+        if (url) {
+          downloadByUrl(url, this.filename || this.download)
+            .then(() => this.$emit('success'))
+            .catch((...err) => this.$emit('error', ...err));
+        } else {
+          console.warn('no url provide');
         }
       },
-    };
-  },
-});
+    },
+  });
 </script>
-
-<template>
-  <a @click="onDownload"><slot></slot></a>
-</template>
